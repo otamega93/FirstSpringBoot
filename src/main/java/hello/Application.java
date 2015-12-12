e74ef3ccbb899859030a61365c54d0788be34ead
Otamega <otamega.93@gmail.com>
2015-12-11 20:03:48 -0430

    Little tests with the aplication.properties configuration method.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,12 +3,18 @@ package hello;
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication //Average Spring boot setup
+@SpringBootApplication
+
 public class Application extends SpringBootServletInitializer {
 
     @Override
