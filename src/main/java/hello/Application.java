d9fd08850d78e1b0486db8e241f3aec08ca51616
Otamega <otamega.93@gmail.com>
2016-04-04 10:13:16 -0430

    comment deleted

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -24,15 +24,6 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-    /*
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    */
     }
 
 }
